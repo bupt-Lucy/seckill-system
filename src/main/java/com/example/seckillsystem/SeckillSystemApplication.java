5d66267fbaf81b348012d33247a3ccdb4ca6bd54
Lucy <2272013306@qq.com>
2025-09-23 10:48:51 +0800

    Initial commit

---
+++ src/main/java/com/example/seckillsystem/SeckillSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.seckillsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seckillsystem/SeckillSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seckillsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
